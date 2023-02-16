13f0f1025c46b3e7434d74ecf9c046983cc93656
Scheuer Mark <markscheuer2002@gmail.com>
2023-02-16 22:02:40 +0100

    Change to api, shows branch names as nodes

--- branch-display-frontend/src/app/branch-history/branch-history.component.ts
+++ branch-display-frontend/src/app/branch-history/branch-history.component.ts
@@ -12,27 +12,25 @@ import { Network } from 'vis';
 export class BranchHistoryComponent implements OnInit {
   @ViewChild('branchHistory', { static: true }) branchHistory!: ElementRef;
 
+  constructor(private gitApiService: GitApiService) {}
+
   ngOnInit() {
-    // create a new Vis.js DataSet to hold the data for our branch history
-    const data = new DataSet();
-    const edges = new DataSet<Edge>();
+   // create a new Vis.js DataSet to hold the data for our branch history
+   const data = new DataSet();
+   const edges = new DataSet<Edge>();
 
-    // add nodes to the DataSet
-    data.add({ id: 1, label: 'master' });
-    data.add({ id: 2, label: 'feature-1' });
-    data.add({ id: 3, label: 'feature-2' });
-    data.add({ id: 4, label: 'feature-3' });
-    data.add({ id: 5, label: 'hotfix-1' });
+   // get the branch history from the API
+   this.gitApiService.getBranchHistory("https://github.com/ScheuerMark/test-branches").subscribe((branchHistory: any) => {
+     // create nodes for each branch
+     for (const branch of branchHistory) {
+       data.add({ id: branch.name, label: branch.name });
+     }
 
-    // add edges to the DataSet to define the relationships between the nodes
-    edges.add({ from: 1, to: 2 });
-    edges.add({ from: 1, to: 3 });
-    edges.add({ from: 2, to: 4 });
-    edges.add({ from: 1, to: 5 });
 
-    // create a new Vis.js Network instance and attach it to the branchHistory element
-    const options = {};
-    const network = new Network(this.branchHistory.nativeElement, {nodes:data, edges:edges}, options);
+     // create a new Vis.js Network instance and attach it to the branchHistory element
+     const options = {};
+     const network = new Network(this.branchHistory.nativeElement, { nodes: data, edges: edges }, options);
+   });
   }
 }
 
